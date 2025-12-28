809d8d571ff796e66c29789e19a7ed355e95c937
Addis Negash <anegash154@gmail.com>
2025-12-28 11:35:08 +0300

    refactor(AppErrors): change InvalidIndexErro to InvalidIndexError

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,13 +1,13 @@
 import { DSError } from "./DSError.js";
 import {EmptyStructureError} from "./EmptyStructureError.js"
 import { InvalidOperatorError } from "./InvalidOperatorError.js";
-import {InvalidIndexErro} from "./InvalidIndexError.js"
+import {InvalidIndexError} from "./InvalidIndexError.js"
 import { ValueNotFoundError } from "./ValueNotFoundError.js";
 
  const AppErrors={
     DSError,
     EmptyStructureError,
-    InvalidIndexErro,
+    InvalidIndexError,
     InvalidOperatorError,
     ValueNotFoundError
  }
