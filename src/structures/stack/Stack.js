123ae170acac7b819913b21a0c6789da5ef76df7
Addis Negash <anegash154@gmail.com>
2025-12-28 14:28:22 +0300

    feat: implement pop method to Stack class

--- src/structures/stack/Stack.js
+++ src/structures/stack/Stack.js
@@ -14,5 +14,12 @@ class Stack{
         this.#stack.push(value);
         return this.#stack.length;
     }
+    pop(){
+        if(this.isEmpty){
+            throw new AppErrors.EmptyStructureError(`Operation not permitted on empty ${this.constructor.name}`);
+        }
+        let data=this.#stack.pop();
+        return data;
+    }
 }
-  
+ 
