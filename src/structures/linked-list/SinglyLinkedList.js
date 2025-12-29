b45597bada427e10757cb77f99010f8a5fad262b
Addis Negash <anegash154@gmail.com>
2025-12-29 15:34:18 +0300

    feat: add removeEnd method to SinglyLinkedList class

--- src/structures/linked-list/SinglyLinkedList.js
+++ src/structures/linked-list/SinglyLinkedList.js
@@ -1,77 +1,100 @@
 import { AppErrors } from "../../errors/index.js";
- 
-class Node{
-    constructor(value){
-        this.value=value;
-        this.next=null;
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
 }
-class SinglyLinkedList{
+class SinglyLinkedList {
     #head;
     #length;
-    constructor(){
-        this.#head=null;
-        this.#length=0;
+    constructor() {
+        this.#head = null;
+        this.#length = 0;
     }
-    get isEmpty(){
-        if(!this.#head){
+    get isEmpty() {
+        if (!this.#head) {
             return true;
         }
         return false;
     }
-    append(value){
-        const newNode=new Node(value);
-        if(this.isEmpty){
-           this.#head=newNode;
+    append(value) {
+        const newNode = new Node(value);
+        if (this.isEmpty) {
+            this.#head = newNode;
         }
-        else{
-            let temp=this.#head;
-            while(temp.next){
-                temp=temp.next;
+        else {
+            let temp = this.#head;
+            while (temp.next) {
+                temp = temp.next;
             }
-            temp.next=newNode;
+            temp.next = newNode;
         }
         return ++this.#length;
     }
-   prepend(value){
-    const newNode=new Node(value);
-    newNode.next=this.#head;
-    this.#head=newNode;
-    return ++this.#length;
-   }
-   insertAt(value, index){
-    if(!Number.isInteger(index)){
-        throw new AppErrors.InvalidIndexError(` "${index}"  index value must be integer`);
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.#head;
+        this.#head = newNode;
+        return ++this.#length;
     }
-    if(index<0 || index>this.#length){
-        throw new AppErrors.InvalidIndexError(` "${index}"  index value must greater or equal to 0 and less or equal to ${this.#length}`);
+    insertAt(value, index) {
+        if (!Number.isInteger(index)) {
+            throw new AppErrors.InvalidIndexError(` "${index}"  index value must be integer`);
+        }
+        if (index < 0 || index > this.#length) {
+            throw new AppErrors.InvalidIndexError(` "${index}"  index value must greater or equal to 0 and less or equal to ${this.#length}`);
+        }
+        if (index === 0) {
+            return this.prepend(value);
+        }
+        else if (index === this.#length) {
+            return this.append(value);
+        }
+        else {
+            const newNode = new Node(value);
+            let temp = this.#head;
+            for (let i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+            return ++this.#length;
+        }
     }
-    if(index===0){
-        return this.prepend(value);
+    removeStart() {
+        if (this.isEmpty) {
+            throw new AppErrors.EmptyStructureError(`Operation not permitted on empty ${this.constructor.name}`);
+        }
+        let removedValue = this.#head.value;
+        this.#head = this.#head.next;
+        --this.#length;
+        return removedValue;
     }
-    else if(index===this.#length){
-        return this.append(value);
-    }
-    else{
-        const newNode=new Node(value);
+    removeEnd() {
+        if (this.isEmpty) {
+            throw new AppErrors.EmptyStructureError(`Operation not permitted on empty ${this.constructor.name}`);
+        }
         let temp=this.#head;
-        for(let i=0;i<index-1;i++){
+        let priv=null;
+        while(temp.next){
+            priv=temp;
             temp=temp.next;
         }
-        newNode.next=temp.next;
-        temp.next=newNode;
-        return ++this.#length;
+        if(priv){
+            let removedValue=priv.next.value;
+            priv.next=null;
+            --this.#length;
+            return removedValue;
+        }
+        else{
+            let removedValue=temp.value;
+            this.#head=null;
+            temp=null;
+            --this.#length;
+            return removedValue;
+        }
     }
-   }
-   removeStart(){
-    if(this.isEmpty){
-        throw  new AppErrors.EmptyStructureError(`Operation not permitted on empty ${this.constructor.name}`);
-    }
-    let removedValue=this.#head.value;
-    this.#head=this.#head.next;
-    --this.#length;
-    return removedValue;
-   }
 }
  
-  
