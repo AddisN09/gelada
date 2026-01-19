f6b93f63e542e33d5fda05bbd431473fbb2a98f1
Addis Negash <anegash154@gmail.com>
2026-01-19 10:46:06 +0300

    feat: add DS namespace to export all data structures

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import{Stack} from './structures/stack/Stack.js';
+import{SinglyLinkedList} from './structures/linked-list/SinglyLinkedList.js';
+import{Queue} from './structures/queue/Queue.js';
+ export const DS={
+    Stack,
+    Queue,
+    SinglyLinkedList
+ }
